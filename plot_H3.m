e37b0426e3d5857f0345dbddb355783cd79f4c10
Josephine <jossan_cuellar@hotmail.com>
2014-12-16 22:29:50 +0100

    Changes in plot

--- plot_H3.m
+++ plot_H3.m
@@ -1,4 +1,5 @@
 %% Homeproblem 3b
+% Task 1
 
 clc
 clear all
@@ -11,9 +12,22 @@ y = linspace(0,1,length(phi));
 figure(1);
 plot3(x,y,phi);
 
+%% Task 2
+% Plot the obtained result and compare it to the result from E6
+clc
+
+e6 = dlmread('phi_E6.data');
+xE6 = linspace(0, length(e6), length(e6));
+
+figure(2);
+clf
+plot(xE6,e6);
+xlim([0 length(e6)]);
+
 
 %% Task 3
-% Plot the exact solution and compare with the obtained result
+% Plot the exact solution and compare with the obtained result from the
+% simulation
 
 exact = dlmread('exact.data');
 phi = dlmread();
